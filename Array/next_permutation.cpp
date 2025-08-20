46d337bfb0eec8ac37232737f2c36a5838eb7544
Musharraf Abdullah <mushabd.22@gmail.com>
2025-08-20 21:03:36 +0530

    array, next permutaion

---
+++ Array/next_permutation.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int idx = -1;
+        int n = nums.size();
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for (int i = n - 1; i > idx; i--) {
+
+            if (nums[i] > nums[idx]) {
+                swap(nums[i], nums[idx]);
+                break;
+            }
+        }
+        
+        reverse((nums.begin() + idx + 1), nums.end());
+
+        return;
+    }
+};
+
+
+int main() {
+
+    Solution sol;
+    vector<int> nums = {1, 2, 3};
+    sol.nextPermutation(nums);
+    cout<< "Original array: ";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    cout << "Next permutation: ";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    return 0;
+
+}
